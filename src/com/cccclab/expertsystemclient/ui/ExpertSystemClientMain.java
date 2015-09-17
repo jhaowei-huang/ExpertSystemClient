99e403c366040793fce1244aa523a9837e74c8fd
JhaoWeiHuang <jhaowei.huang@gmail.com>
2015-09-17 16:38:48 +0800

    tab change event

--- src/com/cccclab/expertsystemclient/ui/ExpertSystemClientMain.java
+++ src/com/cccclab/expertsystemclient/ui/ExpertSystemClientMain.java
@@ -3,19 +3,22 @@ package com.cccclab.expertsystemclient.ui;
 import com.cccclab.expertsystemclient.design.ExpertSystemClientMainDesign;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
+import com.vaadin.ui.TabSheet.SelectedTabChangeListener;
 
 public class ExpertSystemClientMain extends ExpertSystemClientMainDesign {
 	public ExpertSystemClientMain() {
 		this.leftMenu.addTab(new Button("Btn1-1"), "Tab 1");
-		this.leftMenu.addTab(new Button("Label 22"), "Tab 2");
-		this.leftMenu.addTab(new Button("Label 33"), "Tab 3");
+		this.leftMenu.addTab(new Button("Btn1-2"), "Tab 2");
+		this.leftMenu.addTab(new Button("Btn1-3"), "Tab 3");
 
-//		this.leftMenu.addSelectedTabChangeListener(new SelectedTabChangeListener() {
-//		      public void selectedTabChange(
-//		          SelectedTabChangeEvent event) {
-//		        Notification.show("You are watching "
-//		            + event.getTabSheet().getSelectedTab());
-//		      }
-//		    });
+		this.leftMenu.addSelectedTabChangeListener(new SelectedTabChangeListener() {
+			@Override
+			public void selectedTabChange(SelectedTabChangeEvent event) {
+				// TODO Auto-generated method stub
+				Notification.show("current " + event.getTabSheet().getTab(event.getTabSheet().getTabIndex()).getCaption());
+			}
+		});
 	}
 }
